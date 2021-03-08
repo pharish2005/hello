82518ab6d5b4ece04aa4bbc5d6c1f2bdd5771ac0
root <root@ip-172-31-91-136.ec2.internal>
2021-03-08 23:21:32 +0000

    first commit

---
+++ sample/src/main/java/ibm/devops/App.java
@@ -0,0 +1,14 @@
+package ibm.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
